d78dac67207a0dc1645f42fd6f687ef76af687cb
Andrey <yyootttaa@gmail.com>
2024-11-08 20:53:48 +0300

    Fixed diagnostics module

--- src/diagnostic_module.cpp
+++ src/diagnostic_module.cpp
@@ -1,24 +1,17 @@
 #include <chrono>
 #include <memory>
 #include <string>
-// #include <telemetry.h>
-// #include <mavsdk/plugins/telemetry/telemetry.h>
-// #include <mavsdk/mavsdk.h>
 #include <vector>
 #include <iostream>
 #include "yaml-cpp/yaml.h"
-
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
-// #include <mavsdk.h>
-
 #include "diagnostic_msgs/msg/diagnostic_status.hpp"
 #include "diagnostic_msgs/msg/diagnostic_array.hpp"
 #include "diagnostic_msgs/msg/key_value.hpp"
 #include <map>
 
 using namespace std::chrono_literals;
-// using mavsdk::Telemetry, mavsdk::Mavsdk;
 using std::placeholders::_1, std::placeholders::_2;
 
 class Diagnostics : public rclcpp::Node
@@ -32,28 +25,19 @@ public:
         this->declare_parameter("path", default_path_config);
         this->get_parameter_or("path", path_config, default_path_config);
 
-        config = YAML::LoadFile(path_config);
-
-        // auto timer_callback = [this]() -> void 
-        //   {
-        //     std::string path_config = "/home/max/uav_ws/src/diagnostic_status_ros2/config/config.yml";
-        //     config = YAML::LoadFile(path_config);
-            
-        //     RCLCPP_ERROR_STREAM(this->get_logger(), "gps_info: analyzed_variables " << config["gps_info"]["analyzed_variables"].as<std::string>());
-        //     RCLCPP_ERROR_STREAM(this->get_logger(), "gps_info: fallback_protocol " << config["gps_info"]["fallback_protocol"].as<std::string>());
-        //     RCLCPP_ERROR_STREAM(this->get_logger(), "gps_info: importance " << config["gps_info"]["importance"].as<std::string>());
-
-        //     RCLCPP_ERROR_STREAM(this->get_logger(), "fcu_health: analyzed_variables " << config["fcu_health"]["analyzed_variables"].as<std::string>());
-        //     RCLCPP_ERROR_STREAM(this->get_logger(), "fcu_health: fallback_protocol " << config["fcu_health"]["fallback_protocol"].as<std::string>());
-        //     RCLCPP_ERROR_STREAM(this->get_logger(), "fcu_health: importance " << config["fcu_health"]["importance"].as<std::string>());
-
-        //   };
+        try {
+            config = YAML::LoadFile(path_config);
+        } catch (const YAML::BadFile& e) {
+            RCLCPP_ERROR_STREAM(this->get_logger(), "Failed to load YAML file: " << e.what());
+            return;
+        }
 
         timer_ = this->create_wall_timer(500ms, std::bind(&Diagnostics::timer_callback, this));
 
         diagnostics_sub = this->create_subscription<diagnostic_msgs::msg::DiagnosticStatus>("diagnostics", rclcpp::QoS(rclcpp::KeepLast(10)).best_effort().durability_volatile(), std::bind(&Diagnostics::diagnostic_cb, this, _1));
         diagnostic_array_pub = this->create_publisher<diagnostic_msgs::msg::DiagnosticArray>("diagnostic_info_output", rclcpp::QoS(rclcpp::KeepLast(10)).best_effort().durability_volatile());
         diagnostics_pub = this->create_publisher<std_msgs::msg::String>("diagnostics/emergency_command", 10);
+        unknown_diagnostics_sub = this->create_publisher<std_msgs::msg::String>("diagnostics/unknown_keys", 10);
     }
 
 private:
@@ -61,19 +45,17 @@ private:
     rclcpp::TimerBase::SharedPtr timer_;
 
     rclcpp::Subscription<diagnostic_msgs::msg::DiagnosticStatus>::SharedPtr diagnostics_sub;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr unknown_diagnostics_sub;
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr diagnostics_pub;
     rclcpp::Publisher<diagnostic_msgs::msg::DiagnosticArray>::SharedPtr diagnostic_array_pub;
 
-
     YAML::Node config;
-//   rclcpp::SubscriptionOptions sub_options;
-
 
     void timer_callback()
     {
         diagnostic_msgs::msg::DiagnosticArray array_msg;
         array_msg.header.stamp = this->get_clock()->now();
-        
+
         for (auto module_iter = modules_diagnostics.begin(); module_iter != modules_diagnostics.end(); module_iter++)
         {
             for (auto submodule_iter = module_iter->second.begin(); submodule_iter != module_iter->second.end(); submodule_iter++)
@@ -85,13 +67,28 @@ private:
         diagnostic_array_pub->publish(array_msg);
     }
 
+    std::string safe_get_string(const YAML::Node& node, const std::string& key, std::string module_name)
+    {
+        if (node[key]) {
+            return node[key].as<std::string>();
+        } else {
+            RCLCPP_ERROR_STREAM(this->get_logger(), "Missing key: " << key);
+            std_msgs::msg::String msg;
+            msg.data = module_name;
+            unknown_diagnostics_sub->publish(msg);
+            return "";
+        }
+    }
 
     void diagnostic_cb(diagnostic_msgs::msg::DiagnosticStatus diagnostic_status)
     {
-        if (config[diagnostic_status.name]["analyzed_variables"].as<std::string>() == "null")
+        std::string analyzed_variables = safe_get_string(config[diagnostic_status.name], "analyzed_variables", diagnostic_status.name);
+        std::string importance = safe_get_string(config[diagnostic_status.name], "importance", diagnostic_status.name);
+        std::string fallback_protocol = safe_get_string(config[diagnostic_status.name], "fallback_protocol", diagnostic_status.name);
+
+        if (analyzed_variables == "null")
         {
-            // Чекаем статус
-            if (config[diagnostic_status.name]["importance"].as<std::string>() == "executive")
+            if (importance == "executive")
             {
                 std::string total_status = "OK";
                 for (const auto & [key,value]: diagnostic_status.values)
@@ -103,18 +100,14 @@ private:
                 }
                 if (total_status == "ERROR")
                 {
-                    //паблишим команду из config[diagnostic_status.name]["fallback_protocol"]
-                    std_msgs::msg::String pub = std_msgs::msg::String(); 
-                    pub.data = config[diagnostic_status.name]["fallback_protocol"].as<std::string>();
+                    std_msgs::msg::String pub = std_msgs::msg::String();
+                    pub.data = fallback_protocol;
                     this->diagnostics_pub->publish(pub);
                 }
             }
         }
-
-        else 
+        else
         {
-            // std::map<std::string, std::string> analyzed_variables = config[diagnostic_status.name]["analyzed_variables"].as<std::map<std::string, std::string>>();
-            // Чекаем переменные в analyzed_variables
             std::string total_status = "OK";
             for (const auto & [key,value]: diagnostic_status.values)
             {
@@ -133,9 +126,8 @@ private:
             }
             if (total_status == "ERROR")
             {
-                //паблишим команду из config[diagnostic_status.name]["fallback_protocol"]
-                std_msgs::msg::String pub = std_msgs::msg::String(); 
-                pub.data = config[diagnostic_status.name]["fallback_protocol"].as<std::string>();
+                std_msgs::msg::String pub = std_msgs::msg::String();
+                pub.data = fallback_protocol;
                 this->diagnostics_pub->publish(pub);
             }
         }
